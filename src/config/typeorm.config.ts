bb38dbca8792f8a1385eeb86b3812b85c19c7dc9
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-06-04 19:26:23 -0300

    feat: add TypeORM configuration for PostgreSQL database connection

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,23 @@
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import { User } from '../entities/user.entity';
+import { Wallet } from '../entities/wallet.entity';
+import { Transaction } from '../entities/transaction.entity';
+
+config();
+
+const configService = new ConfigService();
+
+export default new DataSource({
+  type: 'postgres',
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_DATABASE'),
+  entities: [User, Wallet, Transaction],
+  migrations: ['src/migrations/*.ts'],
+  migrationsTableName: 'migrations',
+  synchronize: false,
+});
