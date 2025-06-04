3370b1d832397c08e54c7c05f52269b3a35b97f8
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-06-04 19:26:45 -0300

    feat: add Wallet entity with fields for id, balance, userId, and relationship to User

---
+++ src/entities/wallet.entity.ts
@@ -0,0 +1,32 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import { User } from './user.entity';
+
+@Entity('wallets')
+export class Wallet {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('decimal', { precision: 10, scale: 2, default: 0 })
+  balance: number;
+
+  @Column()
+  userId: string;
+
+  @OneToOne(() => User, (user) => user.wallet)
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
