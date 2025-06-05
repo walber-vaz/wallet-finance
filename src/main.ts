30f1176c76638f342909f1fe12c38a35e93c5d5a
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-06-05 08:11:14 -0300

    feat: add helmet and compression middleware for enhanced security and performance

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
+import * as compression from 'compression';
+import { ConsoleLogger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      colors: true,
+      json: true,
+    }),
+  });
 
   app.enableShutdownHooks();
   app.setGlobalPrefix('api/v1');
   app.enableCors();
+  app.use(helmet());
+  app.use(
+    compression({
+      level: 6,
+      threshold: 1024,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Wallet Finance API')
